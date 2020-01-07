1991ce8778f812ac04c00961eff059d0793d3340
Adebayo Ajagunna <adebayoajagunna@gmail.com>
2020-01-06 22:49:05 -0500

    push understanding hooks and state

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React,{useState} from "react";
 
-import GoalList from './components/GoalList/GoalList';
-import NewGoal from './components/NewGoal/NewGoal';
-import './App.css';
+import GoalList from "./components/GoalList/GoalList";
+import NewGoal from "./components/NewGoal/NewGoal";
+import "./App.css";
 
 const App = () => {
-    const yearGoals = [
-    { id: 'yg1', text: 'Believing and having faith in God' },
-    { id: 'yg2', text: 'Study as much materials as possible'},
-    { id: 'yg3', text: 'Get an Internship'},
-    { id: 'yg4', text: 'Maintain a good GPA no matter what!' },
-    { id: 'yg5', text: 'Stay out of trouble' },
-    { id: 'yg6', text: 'Continue to make everyone proud' }
-  ];
+  const [yearGoals, setYearGoals]=useState([
+    { id: "yg1", text: "Believing and having faith in God" },
+    { id: "yg2", text: "Study as much materials as possible" },
+    { id: "yg3", text: "Get an Internship" },
+    { id: "yg4", text: "Maintain a good GPA no matter what!" },
+    { id: "yg5", text: "Stay out of trouble" },
+    { id: "yg6", text: "Continue to make everyone proud" }
+  ]);
 
   const addNewGoalHandler = newGoal => {
-    yearGoals.push(newGoal);
-    console.log(yearGoals);
+    //setYearGoals(yearGoals.concat(newGoal));
+    setYearGoals(prevYearGoals =>prevYearGoals.concat(newGoal));
   };
 
   return (
@@ -29,6 +29,3 @@ const App = () => {
 };
 
 export default App;
-
-
-  
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './NewGoal.css';
 
 const NewGoal = props => {
+   const[enteredGoals, setEnteredGoal] = useState('')
   const addGoalHandler = event => {
     event.preventDefault();
 
     const newGoal = {
       id: Math.random().toString(),
-      text: 'My new goal!'
+      text: enteredGoals
     };
-
+    setEnteredGoal('');
     props.onAddGoal(newGoal);
   };
 
+
+  const textChangeHandler = event => {
+    setEnteredGoal(event.target.value)
+  }
+
   return (
     <form className="new-goal" onSubmit={addGoalHandler}>
-      <input type="text" />
+      <input type="text" value={enteredGoals}onChange={textChangeHandler}/>
       <button type="submit">Add Goal</button>
     </form>
   );
